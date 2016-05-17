df559887359d7091e7a46d5f02e2eea27ff44381
Yoshito Komatsu <ykomatsu@akaumigame.org>
2016-05-17 07:46:00 +0000

    Update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject selfhosted-cljs-example "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://github.com/ykomatsu/selfhosted-cljs-example"
+  :url "https://github.com/ykomatsu/selfhosted-cljs-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
